9360364bcce600034787a86d0d0311ff5dbe22af
KittyCatOfMagic27 <kittycatofmagic@gmail.com>
2025-10-16 14:41:14 -0500

    Changelog and Update

--- KCompilerRust/target/debug/deps/thiserror-676135e79f23efb4.d
+++
@@ -1,9 +0,0 @@
-/home/katluvsya/old_root/KVM_8BIT/KCompilerRust/target/debug/deps/libthiserror-676135e79f23efb4.rmeta: /home/katluvsya/.cargo/registry/src/index.crates.io-6f17d22bba15001f/thiserror-1.0.69/src/lib.rs /home/katluvsya/.cargo/registry/src/index.crates.io-6f17d22bba15001f/thiserror-1.0.69/src/aserror.rs /home/katluvsya/.cargo/registry/src/index.crates.io-6f17d22bba15001f/thiserror-1.0.69/src/display.rs
-
-/home/katluvsya/old_root/KVM_8BIT/KCompilerRust/target/debug/deps/libthiserror-676135e79f23efb4.rlib: /home/katluvsya/.cargo/registry/src/index.crates.io-6f17d22bba15001f/thiserror-1.0.69/src/lib.rs /home/katluvsya/.cargo/registry/src/index.crates.io-6f17d22bba15001f/thiserror-1.0.69/src/aserror.rs /home/katluvsya/.cargo/registry/src/index.crates.io-6f17d22bba15001f/thiserror-1.0.69/src/display.rs
-
-/home/katluvsya/old_root/KVM_8BIT/KCompilerRust/target/debug/deps/thiserror-676135e79f23efb4.d: /home/katluvsya/.cargo/registry/src/index.crates.io-6f17d22bba15001f/thiserror-1.0.69/src/lib.rs /home/katluvsya/.cargo/registry/src/index.crates.io-6f17d22bba15001f/thiserror-1.0.69/src/aserror.rs /home/katluvsya/.cargo/registry/src/index.crates.io-6f17d22bba15001f/thiserror-1.0.69/src/display.rs
-
-/home/katluvsya/.cargo/registry/src/index.crates.io-6f17d22bba15001f/thiserror-1.0.69/src/lib.rs:
-/home/katluvsya/.cargo/registry/src/index.crates.io-6f17d22bba15001f/thiserror-1.0.69/src/aserror.rs:
-/home/katluvsya/.cargo/registry/src/index.crates.io-6f17d22bba15001f/thiserror-1.0.69/src/display.rs:
